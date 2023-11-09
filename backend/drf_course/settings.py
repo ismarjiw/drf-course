b5dfedd6741d6203d289f52c332cc638fbb74788
ismarjiw <ismarjiw@protonmail.com>
2023-11-09 23:18:41 +0000

    add gitpod url to allowed/trusted for admin login

--- backend/drf_course/settings.py
+++ backend/drf_course/settings.py
@@ -30,12 +30,11 @@ INSTALLED_APPS = [
     'rest_framework.authtoken', #Used to enable token authentication
     'core',
     'ecommerce', #New app
-    'corsheaders',
 ]
 
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -144,4 +143,8 @@ REST_FRAMEWORK = {
     'TEST_REQUEST_DEFAULT_FORMAT': 'vnd.api+json'
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "https://8000-ismarjiw-drfcourse-mzwaku5bruw.ws-us106.gitpod.io"
+]
+
+CSRF_TRUSTED_ORIGINS = ['https://8000-ismarjiw-drfcourse-mzwaku5bruw.ws-us106.gitpod.io']
